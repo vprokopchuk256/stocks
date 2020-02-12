ff5f1f299d47759dc8138d2f245792852cd48e7a
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2020-02-11 18:52:39 -0600

    add chart

--- client/src/Details.js
+++ client/src/Details.js
@@ -1,15 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Chart from 'chart.js';
+import moment from 'moment';
 
 import client from './client';
 
+const HistoryChart = ({ history }) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      new Chart(document.getElementById('myChart').getContext('2d'), {
+        type: 'line',
+        title: {
+          display: false
+        },
+        data: {
+            labels: history.map(hi => moment(hi.date).format('D MMM')),
+            datasets: [{
+                data: history.map(hi => hi.value),
+                fill: 'none',
+                lineTension: 0.5,
+                pointRadius: 2,
+                borderWidth: 2
+            }]
+        },
+        options: {
+            legend: {
+              display: false,
+            },
+            scales: {
+                xAxes: [{
+                    position: 'bottom',
+                    gridLines: {
+                      color: '#E3E4E6',
+                      lineWidth: 1,
+                      drawTicks: false,
+                    },
+                    ticks: {
+                      fontSize: '14',
+                      fontColor: '#616161',
+                      padding: 5, 
+                    }
+                }]
+            }
+        }
+      });
+    };
+  }, [history]);
+
+  return (
+    <canvas id='myChart' ref={canvasRef} />
+  )
+};
 export const Details = ({ symbol }) => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
       const res = await client.stock(symbol)
-
-      console.log(res.data);
 
       setData(res.data);
     };
@@ -18,9 +66,8 @@ export const Details = ({ symbol }) => {
   }, [symbol]);
 
   return (
-    <div>
-      <div>{data.symbol}</div>
-      <div>{data.company_name}</div>
+    data && <div>
+      <HistoryChart history={data.history} />
     </div>
   );
 }
